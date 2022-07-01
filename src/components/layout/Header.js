dd2287b0e6b1473b203afcf023568674a491c86f
Mario David <mario@die-davids.de>
2022-07-01 09:53:08 +0200

    book a visit button

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -120,7 +120,7 @@ const Header = ({
                       className="list-reset header-nav-right"
                     >
                       <li>
-                        <Link to="#0" className="button button-primary button-wide-mobile button-sm" onClick={closeMenu}>Sign up</Link>
+                        <Link to="#0" id="book-a-visit" className="button button-primary button-wide-mobile button-sm" onClick={closeMenu}>Book a Visit</Link>
                       </li>
                     </ul>}
                 </div>
